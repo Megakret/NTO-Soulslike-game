61e06f136fb02033f63fdd16a2f14b20b63cde00
Megakret <tymatee@gmail.com>
2022-12-08 00:53:10 +0200

    Добавлено копье и приукрашена шкала

--- NTO_Soulslike/Assets/Scripts/PlayerScripts/Dodging.cs
+++ NTO_Soulslike/Assets/Scripts/PlayerScripts/Dodging.cs
@@ -21,13 +21,13 @@ public class Dodging : MonoCache
             CanDodge = false; // Выключает игроку возможность делать рывок
             PlayerStates.currentState = PlayerStates.States.Dodge; // Дает игроку состояния рывок
             PlayerStates.IFrame = true; // Выдать игроку неуязвимость
-            StartCoroutine(Dodge()); // Сам рывок
+            StartCoroutine(Dodge(DodgeSpeed,DodgeTime)); // Сам рывок
             StartCoroutine(DodgeCdCount()); // Вернуть возможность делать рывок через некоторое время
             StartCoroutine(PlayerStates.ChangeState(DodgeTime, PlayerStates.States.Idle));// Возвращает состояние покоя
             StartCoroutine(IFrameCd());
         }
     }
-    private IEnumerator Dodge()
+    public IEnumerator Dodge(float DodgeSpeed, float DodgeTime)
     {
 
         float StartTime = Time.time;
