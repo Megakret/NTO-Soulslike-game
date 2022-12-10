96eefd86ae92e55f549f781ff3f831ed2a781405
Megakret <tymatee@gmail.com>
2022-12-10 18:25:31 +0200

    Добавлен сундук с заклинанием
    По нажатию кнопки F рядом с сундуком игрок получит заклинание.

--- NTO_Soulslike/Assets/_Scripts/UIScripts/SpellGet.cs
+++ NTO_Soulslike/Assets/_Scripts/UIScripts/SpellGet.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpellGet : MonoBehaviour
+public class SpellGet : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    public Spell spell;
+    public override void Interacting(GameObject plr)
     {
-        
+        ManaHandler manaHandler = plr.GetComponent<ManaHandler>();
+        int index = LastSpellIndex(manaHandler.spells);
+        if(index != -1)
+        {
+            manaHandler.spells[index] = spell;
+            Destroy(gameObject);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public int LastSpellIndex(Spell[] spells)
     {
-        
+        for(int i = 0; i < spells.Length; i++)
+        {
+            if(spells[i] == null)
+            {
+                return i;
+                
+            }
+        }
+        return -1;
     }
 }
